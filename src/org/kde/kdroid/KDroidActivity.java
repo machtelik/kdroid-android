ecc729b48f197ed401baa91020e11e15a9aeb4a4
Mike Achtelik <mike.achtelik+kde@googlemail.com>
2011-08-25 14:32:59 +0200

    Fixed port setting via serviceConnection

--- src/org/kde/kdroid/KDroidActivity.java
+++ src/org/kde/kdroid/KDroidActivity.java
@@ -20,8 +20,12 @@
 package org.kde.kdroid;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.RemoteException;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -32,13 +36,36 @@ public class KDroidActivity extends Activity {
 	private Button saveButton;
 	private EditText portEdit;
 	private Button defaultButton;
+	
+	private KDroidServiceApi api;
+	private boolean isBound = false;
+	
+	private ServiceConnection serviceConnection = new ServiceConnection() {
+		  @Override
+		  public void onServiceConnected(ComponentName name, IBinder service) {
+		    Log.i("KDroid", "Service connection established");
+		 
+		    api = KDroidServiceApi.Stub.asInterface(service);
+		    isBound=true;
+		  }
+		 
+		  @Override
+		  public void onServiceDisconnected(ComponentName name) {
+		    Log.i("KDroid", "Service connection closed");
+		    isBound=false;
+		  }
+		};
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-
-		startService(new Intent(KDroidService.class.getName()));
+		
+		Intent intent = new Intent(KDroidService.class.getName());
+		
+		startService(intent);
+		
+		bindService(intent, serviceConnection, 0);
 
 		portEdit = (EditText) findViewById(R.id.portEdit);
 
@@ -47,6 +74,14 @@ public class KDroidActivity extends Activity {
 
 			@Override
 			public void onClick(View v) {
+				if(isBound) {
+					int port = Integer.parseInt(portEdit.getText().toString());
+					try {
+						api.setPort(port);
+					} catch (RemoteException e) {
+						e.printStackTrace();
+					}
+				}
 			}
 
 		});
@@ -56,7 +91,14 @@ public class KDroidActivity extends Activity {
 
 			@Override
 			public void onClick(View v) {
-				portEdit.setText("48564");
+				if(isBound) {
+					portEdit.setText("48564");
+					try {
+						api.setPort(48564);
+					} catch (RemoteException e) {
+						e.printStackTrace();
+					}
+				}
 			}
 
 		});
@@ -68,7 +110,7 @@ public class KDroidActivity extends Activity {
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
-
+		unbindService(serviceConnection);
 	}
 
 }
